18965c97c25c0d2234f8c6583c5168edf5b23fad
Ionstine.stat <ionstine.stat@gmail.com>
2018-11-12 16:14:56 +0800

    add test.py to run osm data into layers of geojson

---
+++ test.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy
+import json
+from abc import ABCMeta, abstractmethod
+import re
+
+from osm_layers.osm2layers import *
+import imp
+
+if __name__ == "__main__":
+    with open("./map.geojson") as f:
+        tmp = " ".join(f.readlines())
+    fullmap = json.loads(tmp)
+
+    layers_full = osm2layers(fullmap)
+    for layer in layers_full.keys():
+        with open('./data_layers/'+layer+'.geojson','w') as f:
+            json.dump(layers_full[layer],f,indent=2,ensure_ascii=False)
+
